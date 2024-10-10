ca7335079c13bf4e50c7a4907a09d4c02c2969f6
Noah Gruhlke <noahdgruhlke@gmail.com>
2024-10-10 08:16:29 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+[System.AttributeUsage(System.AttributeTargets.All)]
+public class DefaultValueAttribute : Attribute;
+
+namespace StateofVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aArrays = new int[4] {1, 8, 4, 7};
+
+            Console.WriteLine(aArrays[2]);
+
+            //byte defaultByte = default;
+            //short defaultShort = default;
+            //int defaultInt = default;
+            //long defaultLong = default;
+            //float defaultFloat = default;
+            //double defaultDouble = default;
+            //decimal defaultDecimal = default;
+            //bool defaultBool = default;
+            //char defaultChar = default;
+
+            //Console.WriteLine(defaultByte);
+            //Console.WriteLine(defaultShort);
+            //Console.WriteLine(defaultInt);
+            //Console.WriteLine(defaultLong);
+            //Console.WriteLine(defaultFloat);
+            //Console.WriteLine(defaultDouble);
+            //Console.WriteLine(defaultDecimal);
+            //Console.WriteLine(defaultChar);
+            //Console.WriteLine(defaultBool);
+
+            Environment.Exit(0);
+        }
+    }
+}
